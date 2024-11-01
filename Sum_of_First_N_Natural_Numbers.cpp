13e9696c001b2cb58c9adae91e257b8e3ab52e07
VishalPatil <vishalgpatil10@gmail.com>
2024-11-01 23:41:07 +0530

    updated the sum of n number code

--- Sum_of_First_N_Natural_Numbers.cpp
+++ Sum_of_First_N_Natural_Numbers.cpp
@@ -1,3 +1,8 @@
+/**
+ * Program to find sum of first N natural numbers
+ * N Exclusive: (n*(n-1))/2
+ */
+
 #include<iostream>
 using namespace std;
 int main()
@@ -5,6 +10,7 @@ int main()
     int n;
     cout<<"Enter range of natural numbers = "<<endl;
     cin>>n;
-    cout<<"Sum of first N natural numbers is = "<<(n*(n+1))/2<<endl;
+    cout<<"Sum of first inclusive N natural numbers is = "<<(n*(n+1))/2<<endl;
+    cout<<"Sum of first exclusive N natural numbers is = "<<(n*(n-1))/2<<endl;
     return 0;
 }

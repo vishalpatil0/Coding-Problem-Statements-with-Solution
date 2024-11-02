c6e1037c457f292fb47c0545bf5e8fffc24a136d
VishalPatil <vishalgpatil10@gmail.com>
2024-11-02 23:57:43 +0530

    added 2nd approach for rectangle alpha num count

--- Experiment.cpp
+++ Experiment.cpp
@@ -1,24 +1,15 @@
-#include<vector>
 #include<iostream>
 using namespace std;
-void toLower(string &str)
-{
-    for(int i=0;i<str.length();i++)
-    {
-        if(str[i]>=65 && str[i]<=90)
-        {
-            str[i]=str[i]-'A'+'a';
-        }
-    }
-}
+
 int main()
 {
-    string s="aabb";
-    vector<char>chars(s.begin(),s.end());
-    cout<<"chars size --> "<<chars.size()<<endl;
-    for(char x:chars)
-    {
-        cout<<x<<" done "<<endl;
+    
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+           cout<<char('A'+j+(i*3))<<" ";
+        }
+        cout<<endl;
     }
+
     return 0;
 }
--- Patterns/Rectangle Alhpabets 3.cpp
+++ Patterns/Rectangle Alhpabets 3.cpp
@@ -20,5 +20,13 @@ int main()
         cout<<endl;
         i++;
     }
+
+    cout<<"\n\nAnother approach ->"<<endl<<endl;
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+           cout<<char('A'+j+(i*3))<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
